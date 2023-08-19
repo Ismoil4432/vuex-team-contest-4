869bc67251814ad3263587a16b44e13851be79ee
FarruxDEV <farruxbeknozimov@gmail.com>
2023-08-19 13:26:41 +0500

    change some

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: "/",
-			name: "Home",
-			component: () => import("@/views/Home.vue"),
-		},
-		{
-			path: "/courses",
-			name: "Courses",
-			component: () => import("@/views/Courses.vue"),
-		},
-	],
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+  ]
+})
 
-export default router;
+export default router
