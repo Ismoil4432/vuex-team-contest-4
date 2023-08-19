f2a2d39afbdefa84fb62450f924c8cf27b10c595
FarruxDEV <farruxbeknozimov@gmail.com>
2023-08-19 10:36:15 +0500

    add courses page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-  ]
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "Home",
+			component: () => import("@/views/Home.vue"),
+		},
+		{
+			path: "/courses",
+			name: "Courses",
+			component: () => import("@/views/Courses.vue"),
+		},
+	],
+});
 
-export default router
+export default router;
